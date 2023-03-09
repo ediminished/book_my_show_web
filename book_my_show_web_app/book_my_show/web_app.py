c42c67e4dd905ab227d163770c57d1f87603fd98
Akshay Sarbhukan <akgoesdeep@gmail.com>
2023-03-09 22:25:48 +0530

    added the flask web app

--- book_my_show_web_app/book_my_show/web_app.py
+++ book_my_show_web_app/book_my_show/web_app.py
@@ -1,2 +1,17 @@
-if __name__ == "__main__":
-    print("Connected")
+from flask import Flask, jsonify
+
+
+def add_routes(app: Flask):
+    @app.route("/heartbeat")
+    def heart_beat():
+        return jsonify(True)
+
+    @app.route("/")
+    def welcome():
+        return "<h1>Welcome</h1>"
+
+
+def create_app():
+    app = Flask(__name__)
+    add_routes(app=app)
+    return app
